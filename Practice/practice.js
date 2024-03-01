bbdcca23b7b14a44d76d9971e227124b10f65638
abuhureraarain <91952825+abuhureraarain@users.noreply.github.com>
2024-03-01 17:28:41 +0500

    Add files via upload

---
+++ Practice/practice.js
@@ -0,0 +1,49 @@
+"use strict";
+// String data types
+/*console.log("Hello World");
+console.log('Hello World');
+console.log(`Hello World`);
+
+// Number data types
+console.log(1);
+console.log(2);
+console.log(3);
+
+// Boolean data types
+console.log(true);
+console.log(false);
+console.log(2==2);
+
+// Any data types
+console.log("Any data types");
+console.log("Any data types");
+console.log("Any data types");*/
+// Variables declaration: let, const, var,
+// let
+let color1 = "Black";
+let color2 = 'White';
+let color3 = `Red`;
+console.log(color1);
+console.log(color2);
+console.log(color3);
+// const
+const car1 = "Mercedes";
+const car2 = 'Audi';
+const car3 = `BMW`;
+console.log(car1);
+console.log(car2);
+console.log(car3);
+// var
+var city1 = "Moro";
+var city2 = 'Karchi';
+var city3 = `Islamabad`;
+console.log(city1);
+console.log(city2);
+console.log(city3);
+// Variable naming cenversation: Camel Case, Snack Case, Pascal Case,
+let myFirstName = "Abu"; // Camel Case
+let my_first_name = "Abu"; // Snack Case
+let MyFirstName = "Abu"; // Pascal Case
+console.log(myFirstName);
+console.log(my_first_name);
+console.log(MyFirstName);
